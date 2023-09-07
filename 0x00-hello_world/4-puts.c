0f707cd8bb0f01861d5aceff4e377f8d01d735f1
daverobeny <nyamwangedavid2019@gmail.com>
2023-09-07 13:36:07 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main() {
-    puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+/**
+ * main
+ *
+ * Return
+ * /
+int main(void) 
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
