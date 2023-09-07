96986b764564c614dfaa570f3e716e2ff8e1e906
daverobeny <nyamwangedavid2019@gmail.com>
2023-09-07 13:50:18 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,7 @@
  * followed by new line
  * Return: 0 if no errors, return non zero if errors
  */
-int main(void) 
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
