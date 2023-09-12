ea2669434cbfc6ca6404efc8523702ff208c4d29
daverobeny <nyamwangedavid2019@gmail.com>
2023-09-12 13:35:02 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,7 +7,7 @@ void jack_bauer(void)
 {
 	int x = 0;
 	int a = 0, b = 0, c = 0, d = 0;
-	
+
 	while (x < 1440)
 	{
 		_putchar(a + '0');
@@ -16,7 +16,7 @@ void jack_bauer(void)
 		_putchar(c + '0');
 		_putchar(d + '0');
 		_putchar('\n');
-		
+
 		d++;
 		if (d > 9)
 		{
