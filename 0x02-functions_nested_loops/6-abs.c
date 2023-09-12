164c955f5bdccdd4cc08c6be7a2f227510858493
daverobeny <nyamwangedavid2019@gmail.com>
2023-09-12 11:53:19 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _abs - prints abs values
- * @n: stores an absolute
- * return: absolute values
+ * _abs - computes an absolute value of an interger
+ * @n: int type number
+ * return: absolute values of @n
  */
 int _abs(int n)
 {
