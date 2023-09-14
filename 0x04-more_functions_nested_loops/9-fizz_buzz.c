9f687725c31a833da8c7e3ebd6d3674d307cd0aa
daverobeny <nyamwangedavid2019@gmail.com>
2023-09-14 11:41:28 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,45 +2,38 @@
 
 /**
  * main - Entry point of the program
- *
- * Description:
- * This program prints numbers from 1 to 100, replacing multiples of 3 with "Fizz,"
- * multiples of 5 with "Buzz," and multiples of both 3 and 5 with "FizzBuzz."
- * Each number or word is separated by a space, and a newline character is
- * printed at the end of each line.
- *
+ * Description: prints numbers from 1 to 100, replacing multiples of 3 with "Fizz,"
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    for (int i = 1; i <= 100; i++)
-    {
-        if (i % 3 == 0 && i % 5 == 0)
-        {
-            printf("FizzBuzz");
-        }
-        else if (i % 3 == 0)
-        {
-            printf("Fizz");
-        }
-        else if (i % 5 == 0)
-        {
-            printf("Buzz");
-        }
-        else
-        {
-            printf("%d", i);
-        }
-
-        if (i < 100)
-        {
-            printf(" ");
-        }
-        else
-        {
-            printf("\n");
-        }
-    }
-
-    return 0;
+	for (int i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		if (i < 100)
+		{
+			printf(" ");
+		}
+		else
+		{
+			printf("\n");
+		}
+	}
+	
+	return 0;
 }
