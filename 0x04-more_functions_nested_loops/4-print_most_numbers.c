2eb9f0021a5b1ebdbbdffdf1d544193462d6a468
daverobeny <nyamwangedavid2019@gmail.com>
2023-09-14 03:39:33 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * print_most_numbers - Prints numbers from 0 to 9 except for 2 and 4
- * Description: This function prints the numbers from 0 to 9, excluding 2 and 4,
+ * Description: function prints the numbers from 0 to 9, excluding 2 and 4,
  * followed by a newline character.
  */
 void print_most_numbers(void)
@@ -11,7 +11,10 @@ void print_most_numbers(void)
 
 	for (a = 0; (a <= 9 && a != 2 && a != 4); a++)
 	{
-		_putchar(a + '0');
-	}
+		if (a != 2 && a != 4)
+		{
+			_putchar(a + '0');
+		}
 	_putchar('\n');
+	}
 }
