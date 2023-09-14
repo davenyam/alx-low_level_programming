4fc514b7917026b9263f4cd1b138cb058c1d0d02
daverobeny <nyamwangedavid2019@gmail.com>
2023-09-14 10:30:15 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,13 +2,13 @@
 
 /**
  * print_numbers - Prints numbers from 0 to 9
- * Description: This function prints the numbers from 0 to 9 followed by a
+ * Description: function prints numbers from 0 to 9 followed by a
  * newline character.
  */
 void print_numbers(void)
 {
 	int a;
-
+	
 	for (a = 0; a <= 9; a++)
 	{
 		_putchar(a + '0');
