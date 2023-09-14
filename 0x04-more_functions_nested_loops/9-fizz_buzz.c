024ad9bcdf3538a9d6e8e13b937f954a908fd28c
daverobeny <nyamwangedavid2019@gmail.com>
2023-09-14 14:05:55 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,30 +11,19 @@ int main(void)
 
 	for (int i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
-		{
+		if ((i % 3 == 0) && (i % 5 == 0))
 			printf("FizzBuzz");
-		}
 		else if (i % 3 == 0)
-		{
 			printf("Fizz");
-		}
 		else if (i % 5 == 0)
-		{
 			printf("Buzz");
-		}
 		else
-		{
 			printf("%d", i);
-		}
+
 		if (i < 100)
-		{
 			printf(" ");
-		}
 		else
-		{
 			printf("\n");
-		}
 	}
 	
 	return 0;
