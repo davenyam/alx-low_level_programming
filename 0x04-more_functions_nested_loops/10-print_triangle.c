b6620a01185664e92cced67095b023afc054630c
daverobeny <nyamwangedavid2019@gmail.com>
2023-09-15 00:13:18 -0700

    Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -3,8 +3,7 @@
 /**
  * print_triangle - Prints a right-angled triangle made of '#' characters
  * @size: The size of the triangle
- * Description: This function prints a right-angled triangle of '#' characters
- * with a base of 'size' and a height of 'size', followed by a newline character ('\n').
+ * Description: This function prints a right-angled triangle of '#'
  */
 void print_triangle(int size)
 {
