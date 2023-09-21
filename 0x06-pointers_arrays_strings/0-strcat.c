b9aa6fe5026707805df3210d1a665ffed8458e1a
daverobeny <nyamwangedavid2019@gmail.com>
2023-09-21 19:31:35 +0300

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,17 +9,17 @@
  */
 char *_strcat(char *dest, char *src)
 {
-    int len, j;
+	int len, j;
 
-    len = 0;
-    while (dest[len] != '\0')
-    {
-        len++;
-    }
-    for (j = 0; src[j] != '\0'; j++, len++)
-    {
-        dest[len] = src[j];
-    }
-    dest[len] = '\0';
-    return (dest);
+	len = 0;
+	while (dest[len] != '\0')
+	{
+		len++;
+	}
+	for (j = 0; src[j] != '\0'; j++, len++)
+	{
+		dest[len] = src[j];
+	}
+	dest[len] = '\0';
+	return (dest);
 }
