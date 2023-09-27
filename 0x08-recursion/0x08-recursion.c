1afdd87a9c100639ad96c3dac046ee1b6cffb22d
daverobeny <nyamwangedavid2019@gmail.com>
2023-09-27 18:35:33 +0300

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0x08-recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string using recursion
+ *
+ * @s: Pointer to the string to be printed
+ *
+ * Description: This function recursively prints each character
+ * of the string pointed to by 's' followed by a newline character.
+ */
+void _puts_recursion(char *s)
+{
+    if (*s == '\0')
+    {
+        return;
+    }
+    _putchar(*s);
+    _puts_recursion(s + 1);
+}
