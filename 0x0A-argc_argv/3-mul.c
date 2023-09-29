a3c4bb0f341dcbef75e37389535d5fa981c86bd7
daverobeny <nyamwangedavid2014@gmail.com>
2023-09-29 18:31:29 +0300

    3. Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 
 /**
  * main - Multiplies two numbers and prints the result.
@@ -10,19 +9,67 @@
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2, result;
+	int num1 = 0, num2 = 0, result = 0;
+	int i, negative = 0;
 
 	if (argc != 3)
 	{
-		printf("Error\n");
+		for (i = 0; "Error\n"[i]; i++)
+		{
+			_putchar("Error\n"[i]);
+		}
 		return (1);
 	}
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
+	for (i = 0; argv[1][i]; i++)
+	{
+		if (argv[1][0] == '-')
+		{
+			negative = 1;
+			continue;
+		}
+		num1 = num1 * 10 + (argv[1][i] - '0');
+	}
+
+	for (i = 0; argv[2][i]; i++)
+	{
+		if (argv[2][0] == '-')
+		{
+			negative = !negative;
+			continue;
+		}
+		num2 = num2 * 10 + (argv[2][i] - '0');
+	}
+
 	result = num1 * num2;
 
-	printf("%d\n", result);
+	if (negative)
+	{
+		_putchar('-');
+	}
+
+	if (result == 0)
+	{
+		_putchar('0');
+	}
+	else
+	{
+		int divisor = 1000000000;  // Max int value has 10 digits
+		int leadingZero = 1;       // Flag to skip leading zeros
+
+		while (divisor > 0)
+		{
+			int digit = (result / divisor) % 10;
+			if (digit != 0 || !leadingZero)
+			{
+				_putchar(digit + '0');
+				leadingZero = 0;
+			}
+			divisor /= 10;
+		}
+	}
+
+	_putchar('\n');
 
 	return (0);
 }
