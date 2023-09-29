ba2f95368ade24d6fbb2f4657c6009cf6e0b2487
daverobeny <nyamwangedavid2014@gmail.com>
2023-09-29 18:59:06 +0300

    3. Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,43 +9,28 @@
  */
 int main(int argc, char *argv[])
 {
-	int num1 = 0, num2 = 0, result = 0;
-	int i, negative = 0;
+	int num1, num2, result;
 
 	if (argc != 3)
 	{
-		for (i = 0; "Error\n"[i]; i++)
-		{
-			_putchar("Error\n"[i]);
-		}
+		_putchar('E');
+		_putchar('r');
+		_putchar('r');
+		_putchar('o');
+		_putchar('r');
+		_putchar('\n');
 		return (1);
 	}
 
-	for (i = 0; argv[1][i]; i++)
-	{
-		if (argv[1][0] == '-')
-		{
-			negative = 1;
-			continue;
-		}
-		num1 = num1 * 10 + (argv[1][i] - '0');
-	}
-
-	for (i = 0; argv[2][i]; i++)
-	{
-		if (argv[2][0] == '-')
-		{
-			negative = !negative;
-			continue;
-		}
-		num2 = num2 * 10 + (argv[2][i] - '0');
-	}
-
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
 	result = num1 * num2;
 
-	if (negative)
+	/* Handle negative result */
+	if (result < 0)
 	{
 		_putchar('-');
+		result = -result;
 	}
 
 	if (result == 0)
@@ -54,17 +39,17 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
-		int divisor = 1000000000;  // Max int value has 10 digits
-		int leadingZero = 1;       // Flag to skip leading zeros
+		/* Calculate and print digits one by one */
+		int divisor = 1;
+		while (divisor <= result / 10)
+		{
+			divisor *= 10;
+		}
 
 		while (divisor > 0)
 		{
 			int digit = (result / divisor) % 10;
-			if (digit != 0 || !leadingZero)
-			{
-				_putchar(digit + '0');
-				leadingZero = 0;
-			}
+			_putchar('0' + digit);
 			divisor /= 10;
 		}
 	}
