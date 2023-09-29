6d5404e496c5e4b8ba794a6e9bb193a7b4fabe97
daverobeny <nyamwangedavid2019@gmail.com>
2023-09-29 17:01:47 +0300

    0. It ain't what they call you, it's what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,6 +3,9 @@
 /**
  * main - entry point to the program
  *
+ * @argc: The number of command-line arguments
+ * @argv: An array containing the command-line arguments
+ *
  * Return: Always 0 - Success
  */
 int main(int argc, char *argv[])
