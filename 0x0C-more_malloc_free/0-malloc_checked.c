0e6acbecd2d96e3369bff4f58cebbe9039a35f5d
daverobeny <nyamwangedavid2019@gmail.com>
2023-10-05 23:49:19 +0300

    malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -9,13 +9,12 @@
  */
 void *malloc_checked(unsigned int b)
 {
-    void *i = malloc(b);
+	void *i = malloc(b);
 
-    if (i == NULL)
-    {
-        perror("Memory allocation failed");
-        exit(98);
-    }
+	if (i == NULL)
+	{
+		exit(98);
+	}
 
-    return i;
+	return i;
 }
