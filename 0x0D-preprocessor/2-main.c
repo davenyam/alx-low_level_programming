b6640375468cd7b36e8bf79f6d87c4dfcd7d602d
daverobeny <nyamwangedavid2019@gmail.com>
2023-10-10 14:25:07 -0700

    preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * Description: This program prints the name of the current source file.
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
