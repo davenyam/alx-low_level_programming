de80001a90aba77f95aebb745a500274b96dc439
David Robert <itsdavie2014@gmail.com>
2024-04-07 06:14:33 -0700

    operations

---
+++ 0x18-dynamic_libraries/sub.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * sub - substract two numbers
+ * @a: 1st int
+ * @b: 2nd int
+ * return: substraction
+ */
+int sub(int a, int b)
+{
+	return(a - b);
+}
